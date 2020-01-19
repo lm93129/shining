bf3bfc0ab5ee76cbad6aedbeb2eedd434f86ccb6
lm93129 <lm93129@qq.com>
2020-01-19 23:37:31 +0800

    修复一个变量命名错误的问题

--- appdlserver/main.go
+++ appdlserver/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"appdlserver/config"
 	"appdlserver/router"
-	"appdlserver/util"
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 )
 
@@ -27,15 +29,38 @@ func main() {
 	// 装载路由
 	r := router.InitRouter()
 	s := &http.Server{
-		Addr:           fmt.Sprintf(":%s", os.Getenv("PROT")),
+		Addr:           fmt.Sprintf(":%s", os.Getenv("PORT")),
 		Handler:        r,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   15 * time.Second,
-		MaxHeaderBytes: 2 << 20,
+		MaxHeaderBytes: 1 << 20,
 	}
 
-	util.Log().Println("DataCollectApi服务启动\n")
-	if err := s.ListenAndServe(); err != nil {
-		util.Log().Println("DataCollectApi服务关闭\n")
+	fmt.Println("|-----------------------------------|")
+	fmt.Println("|              闪 灵                |")
+	fmt.Println("|-----------------------------------|")
+	fmt.Println("|     Http Server Start Successful  |")
+	fmt.Println("|     Port: " + os.Getenv("PORT") + "  Pid:" + fmt.Sprintf("%d", os.Getpid()) + "         |")
+	fmt.Println("|-----------------------------------|")
+	fmt.Println("")
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("HTTP server listen: %s\n", err)
+		}
+	}()
+
+	signalChan := make(chan os.Signal)
+	signal.Notify(signalChan, os.Interrupt)
+	sig := <-signalChan
+	log.Println("Get Signal:", sig)
+	log.Println("Shutdown Server ...")
+	// 接收到关闭信号后等待3秒退出
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
 	}
+	log.Println("Server exiting")
 }
